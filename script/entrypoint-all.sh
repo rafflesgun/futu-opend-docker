79edb4200ca1904377e53c054f45905a82f50ff2
Raffles Gun <rafflesgun@users.noreply.github.com>
2026-04-23 20:34:34 +0800

    update opend startup params

--- script/entrypoint-all.sh
+++ script/entrypoint-all.sh
@@ -46,7 +46,7 @@ terminate_children() {
 
 run_opend() {
   validate_opend_inputs
-  set -- /usr/local/bin/futu-opend --config "$OPEND_CONFIG_PATH"
+  set -- /usr/local/bin/futu-opend --config "$OPEND_CONFIG_PATH" --rest-keys-file "$KEYS_PATH" --grpc-keys-file "$KEYS_PATH" --ws-keys-file "$KEYS_PATH"
 
   if [ -n "${FUTU_OPEND_DEVICE_ID:-}" ]; then
     set -- "$@" --device-id "$FUTU_OPEND_DEVICE_ID"
@@ -63,7 +63,7 @@ run_mcp() {
 run_setup() {
   validate_opend_inputs
 
-  set -- /usr/local/bin/futu-opend --config "$OPEND_CONFIG_PATH" --rest-keys-file "$KEYS_PATH" --grpc-keys-file "$KEYS_PATH" --ws-keys-file "$KEYS_PATH"
+  set -- /usr/local/bin/futu-opend --config "$OPEND_CONFIG_PATH"
 
   if [ -n "${FUTU_OPEND_DEVICE_ID:-}" ]; then
     set -- "$@" --device-id "$FUTU_OPEND_DEVICE_ID"
@@ -83,7 +83,7 @@ run_both() {
   validate_opend_inputs
   validate_mcp_inputs
 
-  set -- /usr/local/bin/futu-opend --config "$OPEND_CONFIG_PATH"
+  set -- /usr/local/bin/futu-opend --config "$OPEND_CONFIG_PATH" --rest-keys-file "$KEYS_PATH" --grpc-keys-file "$KEYS_PATH" --ws-keys-file "$KEYS_PATH"
 
   if [ -n "${FUTU_OPEND_DEVICE_ID:-}" ]; then
     set -- "$@" --device-id "$FUTU_OPEND_DEVICE_ID"
